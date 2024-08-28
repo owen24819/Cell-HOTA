6ff0a1258429fd0c85e4e0ba18b576a107f3f953
Owen O'Connor <ooconnor@bu.edu>
2024-08-28 16:38:33 -0400

    Added functionality to specify DATASET and MODEL_NAME in CLI which are used to create the GT / TRACKER Folders

--- scripts/run_cells_challenge.py
+++ scripts/run_cells_challenge.py
@@ -16,8 +16,8 @@ Command Line Arguments: Defaults, # Comments
         'OUTPUT_DETAILED': True,
         'PLOT_CURVES': True,
     Dataset arguments:
-        'GT_FOLDER': os.path.join(code_path, 'data/gt/moma/test/test-HOTA/'),  # Location of GT data
-        'TRACKERS_FOLDER': os.path.join(code_path, 'data/trackers/moma/test/test-HOTA/'),  # Trackers location
+        'MODEL_NAME': 'Cell-TRACTR', # Name of the model
+        'DATASET': 'moma', # Dataset to test
         'OUTPUT_FOLDER': None,  # Where to save eval results (if None, same as TRACKERS_FOLDER)
         'TRACKERS_TO_EVAL': None,  # Filenames of trackers to eval (if None, all in folder)
         'CLASSES_TO_EVAL': ['pedestrian'],  # Valid: ['pedestrian']
@@ -33,6 +33,11 @@ Command Line Arguments: Defaults, # Comments
         'SKIP_SPLIT_FOL': False,    # If False, data is in GT_FOLDER/cells-SPLIT_TO_EVAL/ and in
                                     # TRACKERS_FOLDER/cells-SPLIT_TO_EVAL/tracker/
                                     # If True, then the middle 'cells-split' folder is skipped for both.
+
+        # MODEL_NAME and DATASET are used to generate the GT_FOLDER and TRACKERS_FOLDER
+        default_config['GT_FOLDER'] = str(MOT_path / 'data' / default_config['DATASET'] / 'CTC' / 'test-HOTA')  # Location of GT data
+        default_config['TRACKERS_FOLDER'] = str(MOT_path / 'models' / default_config['MODEL_NAME'] / 'results' / default_config['DATASET'] / 'test' / 'HOTA')  # Trackers location
+
     Metric arguments:
         'METRICS': ['HOTA','CLEAR', 'Identity', 'VACE', 'JAndF']
 """
@@ -43,26 +48,15 @@ import argparse
 from pathlib import Path
 from multiprocessing import freeze_support
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+code_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, code_path)
+
 import trackeval  # noqa: E402
 import time
 from trackeval.utils import convert_CTC_to_MOTS
 
-dataset = 'DynamicNuclearNet-tracking-v1_0' # ['moma',''DynamicNuclearNet-tracking-v1_0']
-model = 'delta' # ['cell-trackformer','embedtrack','delta']
-
-gt_path = Path('/projectnb/dunlop/ooconnor/MOT/data') / dataset / 'CTC' / 'test-HOTA'
-res_path = Path('/projectnb/dunlop/ooconnor/MOT/models') / model / 'results' / dataset / 'test' / 'HOTA'
-
-if not res_path.exists():
-    convert_CTC_to_MOTS(hotapath = res_path, ctcpath = (res_path.parent / 'CTC'))
-
-if not gt_path.exists():
-    convert_CTC_to_MOTS(hotapath = gt_path, ctcpath = (gt_path.parents[1] / 'CTC' / 'test'))
-
 flex_div = True
 count_edges = True
-start_time = time.time()
 
 if __name__ == '__main__':
     freeze_support()
@@ -70,7 +64,7 @@ if __name__ == '__main__':
     # Command line interface:
     default_eval_config = trackeval.Evaluator.get_default_eval_config()
     default_eval_config['DISPLAY_LESS_PROGRESS'] = False
-    default_dataset_config = trackeval.datasets.CellsChallenge.get_default_dataset_config(res_path,gt_path)
+    default_dataset_config = trackeval.datasets.CellsChallenge.get_default_dataset_config()
     default_metrics_config = {'METRICS': ['HOTA', 'CLEAR', 'Identity']}
     config = {**default_eval_config, **default_dataset_config, **default_metrics_config}  # Merge default configs
     parser = argparse.ArgumentParser()
@@ -102,16 +96,27 @@ if __name__ == '__main__':
     dataset_config = {k: v for k, v in config.items() if k in default_dataset_config.keys()}
     metrics_config = {k: v for k, v in config.items() if k in default_metrics_config.keys()}
 
+    # Convert results to HOTA format if necessary
+    tracker_path = Path(default_dataset_config['TRACKERS_FOLDER'])
+    if not tracker_path.exists():
+        convert_CTC_to_MOTS(hotapath = tracker_path, ctcpath = (tracker_path.parent / 'CTC'))
+
+    # Convert ground truths to HOTA format if necessary
+    gt_path = Path(default_dataset_config['GT_FOLDER'])
+    if not gt_path.exists():
+        convert_CTC_to_MOTS(hotapath = gt_path, ctcpath = (gt_path.parents[1] / 'CTC' / 'test'))
+
+    start_time = time.time()
     # Run code
     evaluator = trackeval.Evaluator(eval_config)
-    dataset_list = [trackeval.datasets.CellsChallenge(res_path,gt_path,dataset_config)]
+    dataset_list = [trackeval.datasets.CellsChallenge(dataset_config)]
     metrics_list = []
     for metric in [trackeval.metrics.HOTA]:
         if metric.get_name() in metrics_config['METRICS']:
             metrics_list.append(metric(flex_div))
     if len(metrics_list) == 0:
         raise Exception('No metrics selected for evaluation')
-    evaluator.evaluate(dataset_list, metrics_list, flex_div, dataset, count_edges)
+    evaluator.evaluate(dataset_list, metrics_list, flex_div, count_edges)
 
 end_time = time.time()
 diff = end_time - start_time
